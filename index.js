f3e4a706976ba1d4b9d436c9da539028a73de1ea
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-11 14:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ function johnLennonFacts (facts) {
 
 function iLoveTheBeatles (number) {
   var array = [];
-  // do {
-  //   array.push('I love the Beatles!')
-  // // } while (number < 15) {
-  // //   array.push('I love the Beatles!');
-  // //   number++;
-  // }
-  // return array
+  do {
+    array.push('I love the Beatles!');
+    number++
+  } while (number < 15);
+  return array
 }
